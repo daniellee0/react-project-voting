4481f08eb9effa9d8ad71d35fde66ad94b3b618d
Kyle <kyleavalani@msn.com>
2018-11-24 16:39:51 -0800

    Add functionality with PapaParse to read in voterparticipation csv

--- src/LeafletMap.js
+++ src/LeafletMap.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
+import Papa from 'papaparse';
+
+
+let state = {
+  targetYear: [2016],
+  targetFocus: ['Total Voter Turnout'],
+  targetAge: ['18-24']
+};
+
+let parsedDataGlobal = {
+  data: []
+};
 
 export default class LeafletMap extends Component {
   
     render() {
+    let file = "/data/voterparticipation.csv";
+    Papa.parse(file, {
+      header: true,
+      dynamicTyping: true,
+      download: true,
+      trimHeaders: true,
+      complete: function(papaResults) {
+        let papaData = papaResults.data;
+        let targetPapaData = papaData.filter(obj => obj.Age === state.targetAge[0] && obj.Year === state.targetYear[0] && obj.County != 'zWashington State');
+        parsedDataGlobal.data.push(targetPapaData); 
+        console.log(papaData);
+        console.log(targetPapaData);
+      }
+    });
+    console.log(parsedDataGlobal.data);
       return (
         <div>
             
