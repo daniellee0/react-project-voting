f3bb245b5c65175a2dd8be6f63ce5f8cb25831d4
Kyle <kyleavalani@msn.com>
2018-11-25 00:52:31 -0800

    Save progress for the night. Still working on adding counties geojson

--- src/MyMap.js
+++ src/MyMap.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Papa from 'papaparse';
-import {Map, TileLayer} from 'react-leaflet';
+import {Map, TileLayer, GeoJSON} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'
 
 
@@ -14,9 +14,13 @@ let parsedDataGlobal = {
   data: []
 };
 
+let statesGeoJsonData = {
+  data: []
+};
+
 export class MyMap extends Component {
   
-    render() {
+  render() {
     // Parse the csv data into a JSON object and pass to global
     let file = "/data/voterparticipation.csv";
     Papa.parse(file, {
@@ -36,18 +40,34 @@ export class MyMap extends Component {
     // End Parsing
 
       let testViewport = {
-        center: [51.505,-0.09],
-        zoom: 13
+        center: [47.3511, -120.7401],
+        zoom: 7
       }
+
+      function geojsonData() {
+        fetch('/data/combined.geo.json')
+          .then(response => {
+            let dataPromise = response.json();
+            return dataPromise;
+          })
+          .then( jsonData => {
+            statesGeoJsonData.data.push(<GeoJSON data={jsonData} style={{color: '#006400'}} />);
+          }); 
+          return statesGeoJsonData.data;       
+      }
+      console.log(geojsonData());
+      let tempString = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+
       return (
         <Map viewport={testViewport} style={{ width: '100%', height: '600px' }}>
           <TileLayer
             url='https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia3lsZWF2YWxhbmkiLCJhIjoiY2pvdzd3NGtzMGgxMjNrbzM0cGhwajRxNyJ9.t8zAjKz12KLZQ8GLp2hDFQ'
-            attribution= 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+            attribution= {tempString}
             minZoom='1'
             maxZoom='18'
             id='mapbox.streets'
           />
+          {geojsonData()}
         </Map>
       );
     }
