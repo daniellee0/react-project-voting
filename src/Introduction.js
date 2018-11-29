20e0e6928d536153bd699239ac299602196c3f2a
Kyle <kyleavalani@msn.com>
2018-11-28 16:31:04 -0800

    Add some new text

--- src/Introduction.js
+++ src/Introduction.js
@@ -14,15 +14,15 @@ export default class Introduction extends Component {
                         {/* Card should be another component */}
                         <div>
                             <i className="fas fa-pencil-alt fa-3x"></i>
-                            <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p>Take the time to leave a meaningful review about your voting experience. Whether you're on the go, or at home, MyVote provides you with access anywhere and everywhere.</p>
                         </div>
                         <div>
                             <i className="far fa-chart-bar fa-3x"></i>                    
-                            <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p>Review analytics about voter satisfaction in your local/national area. Government entities can utilize this data to develop comprehensive solutions in regard to voting.</p>
                         </div>
                         <div>
                             <i className="fas fa-users fa-3x"></i>
-                            <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p>Analyze voter registration and turnout data for WA State counties across a span of nearly a decade of data. Further filter the data by age to see how frequently different age brackets vote.</p>
                         </div>
                     </div>
                 </div>
