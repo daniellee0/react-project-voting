371cd47cd9f110f3790a4716f96ace06a9d9a66a
Kyle <kyleavalani@msn.com>
2018-11-28 21:20:21 -0800

    Remove unnecessary comments

--- src/Introduction.js
+++ src/Introduction.js
@@ -5,12 +5,11 @@ export default class Introduction extends Component {
         return (
             <div id="introduction" className="content-container">
                 <div>
-                    <h2>Introducing the voter turnout and review platform</h2>
+                    <h2>Introducing the voter turnout and data analysis platform</h2>
                     <p>Leave a review, digest analytics, and see what others have to say.</p>
                 </div>
                 <div className="card-container">
                     <div className="cards">
-                        {/* Card should be another component */}
                         <div>
                             <i className="fas fa-pencil-alt fa-3x"></i>
                             <p>Take the time to leave a meaningful review about your voting experience. Whether you're on the go, or at home, MyVote provides you with access anywhere and everywhere.</p>
