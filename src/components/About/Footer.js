3f7396329820d69568e79460c5d5a3c2592ca72d
Kyle Avalani <kyleavalani@msn.com>
2018-12-07 20:40:31 -0800

    Update icon sizes and fix some spacing issues

--- src/components/About/Footer.js
+++ src/components/About/Footer.js
@@ -6,9 +6,9 @@ export class Footer extends Component {
     render() {
         return (
             <footer>
-                <p>Contact us! E-mail for serious inquires: <a aria-label="Email" href="mailto:votefact340@gmail.com" rel="noopener noreferrer" target="_blank" ><i aria-label="Email" className="fa fa-envelope-o fa-2x" alt="email link"></i></a></p>    
-                <p>Data from Kaggle: <a aria-label="Justice data" href="https://www.kaggle.com/umichigan/court-justices"><i aria-label="Justice data" className="fa fa-database fa-2x" ></i></a> 
-                    &nbsp;&nbsp;and the Secretary of State: <a aria-label="Election data" href="https://www.sos.wa.gov/elections/research/data-and-statistics.aspx"><i aria-label="Election data" className="fa fa-university fa-2x" ></i></a>
+                <p>Contact us! E-mail for serious inquires: <a aria-label="Email" href="mailto:votefact340@gmail.com" rel="noopener noreferrer" target="_blank" ><i aria-label="Email" className="fa fa-envelope-o fa-1x" alt="email link"></i></a></p>    
+                <p>Data from Kaggle: <a aria-label="Justice data" href="https://www.kaggle.com/umichigan/court-justices"><i aria-label="Justice data" className="fa fa-database fa-1x" ></i></a> 
+                    &nbsp;and the WA Secretary of State: <a aria-label="Election data" href="https://www.sos.wa.gov/elections/research/data-and-statistics.aspx"><i aria-label="Election data" className="fa fa-university fa-1x" ></i></a>
                 </p>
                 <p>&copy; Copyright 2018 Kyle Avalani, Daniel Lee, Andrea Koozer, Hilton Vo</p>
             </footer>
